ed4b2f6d6d07601644b5845a80000756a21b220f
Nkot117 <kumakura117@gmail.com>
2024-02-07 21:52:27 +0900

    compileSdkを34に変更

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.nkot.mvvmandrepositorysample"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.nkot.mvvmandrepositorysample"
