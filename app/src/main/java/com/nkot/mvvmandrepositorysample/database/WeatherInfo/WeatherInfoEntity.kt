6ea9c8370fb61f65a18aa54eaee367eadda76502
Nkot117 <kumakura117@gmail.com>
2024-02-12 15:45:00 +0900

    DBモデルとDomainモデルを変換するロジックをModel側に持たせるように修正

--- app/src/main/java/com/nkot/mvvmandrepositorysample/database/WeatherInfo/WeatherInfoEntity.kt
+++ app/src/main/java/com/nkot/mvvmandrepositorysample/database/WeatherInfo/WeatherInfoEntity.kt
@@ -2,6 +2,7 @@ package com.nkot.mvvmandrepositorysample.database.WeatherInfo
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.nkot.mvvmandrepositorysample.domain.DomainWeatherInfo
 import java.util.Date
 
 @Entity(tableName = "weather_info_table")
@@ -11,3 +12,10 @@ data class WeatherInfoEntity(
     val weather: String,
     val created: Date
 )
+
+fun WeatherInfoEntity.asDomainModel(): DomainWeatherInfo {
+    return DomainWeatherInfo(
+        city = city,
+        weather = weather,
+    )
+}
--- app/src/main/java/com/nkot/mvvmandrepositorysample/domain/DomainWeatherInfo.kt
+++ app/src/main/java/com/nkot/mvvmandrepositorysample/domain/DomainWeatherInfo.kt
@@ -1,6 +1,17 @@
 package com.nkot.mvvmandrepositorysample.domain
 
+import com.nkot.mvvmandrepositorysample.database.WeatherInfo.WeatherInfoEntity
+import java.util.Date
+
 data class DomainWeatherInfo(
     val city: String,
     val weather: String,
 )
+
+fun DomainWeatherInfo.asDatabaseModel(): WeatherInfoEntity {
+    return WeatherInfoEntity(
+        city = city,
+        weather = weather,
+        created = Date()
+    )
+}
