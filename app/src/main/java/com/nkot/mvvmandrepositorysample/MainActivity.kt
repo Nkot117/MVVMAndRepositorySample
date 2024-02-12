21f86c6d7e4d88144b6c04a1bb59d06b7b2423c8
Nkot117 <kumakura117@gmail.com>
2024-02-12 15:45:11 +0900

    不要なimportを削除

--- app/src/main/java/com/nkot/mvvmandrepositorysample/MainActivity.kt
+++ app/src/main/java/com/nkot/mvvmandrepositorysample/MainActivity.kt
@@ -4,9 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.nkot.mvvmandrepositorysample.data.WeatherInfoRepository
-import com.nkot.mvvmandrepositorysample.data.cash.WeatherInfoRepositoryLocalCashSource
-import com.nkot.mvvmandrepositorysample.data.remote.WeatherInfoRepositoryRemoteSource
-import com.nkot.mvvmandrepositorysample.network.WeatherInfoApiService
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
--- app/src/main/java/com/nkot/mvvmandrepositorysample/network/WeatherInfoApiService.kt
+++ app/src/main/java/com/nkot/mvvmandrepositorysample/network/WeatherInfoApiService.kt
@@ -1,10 +1,6 @@
 package com.nkot.mvvmandrepositorysample.network
 
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Call
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
