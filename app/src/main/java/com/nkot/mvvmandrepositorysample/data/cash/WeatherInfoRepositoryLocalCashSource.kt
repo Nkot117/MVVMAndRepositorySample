d69f1cb8697089fff2e7642ea06ac107094f3a0d
Nkot117 <kumakura117@gmail.com>
2024-02-12 15:44:09 +0900

    Cashから最近の情報を取得できるように変更

--- app/src/main/java/com/nkot/mvvmandrepositorysample/data/cash/WeatherInfoRepositoryLocalCashSource.kt
+++ app/src/main/java/com/nkot/mvvmandrepositorysample/data/cash/WeatherInfoRepositoryLocalCashSource.kt
@@ -1,18 +1,20 @@
 package com.nkot.mvvmandrepositorysample.data.cash
 
+import com.nkot.mvvmandrepositorysample.data.WeatherInfoRepositoryInterface
 import com.nkot.mvvmandrepositorysample.database.WeatherInfo.WeatherInfoDao
-import com.nkot.mvvmandrepositorysample.database.WeatherInfo.WeatherInfoEntity
+import com.nkot.mvvmandrepositorysample.database.WeatherInfo.asDomainModel
 import com.nkot.mvvmandrepositorysample.domain.DomainWeatherInfo
-import java.util.Date
+import com.nkot.mvvmandrepositorysample.domain.asDatabaseModel
 import javax.inject.Inject
 
-class WeatherInfoRepositoryLocalCashSource @Inject constructor(private val weatherInfoDao: WeatherInfoDao) {
+class WeatherInfoRepositoryLocalCashSource @Inject constructor(private val weatherInfoDao: WeatherInfoDao) :
+    WeatherInfoRepositoryInterface {
     suspend fun insertWeatherInfo(weatherInfo: DomainWeatherInfo) {
-        val info = WeatherInfoEntity(
-            city = weatherInfo.city,
-            weather = weatherInfo.weather,
-            created = Date()
-        )
-        weatherInfoDao.insert(info)
+        weatherInfoDao.insert(weatherInfo.asDatabaseModel())
+    }
+
+    override suspend fun getWeatherInfo(city: String): DomainWeatherInfo {
+        val weatherInfo = weatherInfoDao.getLatestWeatherInfo(city)
+        return weatherInfo.asDomainModel()
     }
 }
