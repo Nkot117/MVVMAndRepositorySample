9f9563c8dacf51aa93a938acbb0c959f5537aa1b
Nkot117 <kumakura117@gmail.com>
2024-02-12 16:12:40 +0900

    不要なimportを削除

--- app/src/main/java/com/nkot/mvvmandrepositorysample/ui/MainActivity.kt
+++ app/src/main/java/com/nkot/mvvmandrepositorysample/ui/MainActivity.kt
@@ -2,15 +2,8 @@ package com.nkot.mvvmandrepositorysample.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.nkot.mvvmandrepositorysample.R
-import com.nkot.mvvmandrepositorysample.data.WeatherInfoRepository
-import com.nkot.mvvmandrepositorysample.data.cash.WeatherInfoRepositoryLocalCashSource
-import com.nkot.mvvmandrepositorysample.data.remote.WeatherInfoRepositoryRemoteSource
 import com.nkot.mvvmandrepositorysample.databinding.ActivityMainBinding
-import com.nkot.mvvmandrepositorysample.network.WeatherInfoApiService
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+
 
 class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
